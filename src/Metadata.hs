13887646206f8ad22e5f7f9360f30c38c086e2a2
cutsea110 <cutsea110@gmail.com>
2013-08-09 21:46:12 +0900

    Add op function as Op

--- src/Metadata.hs
+++ src/Metadata.hs
@@ -1,5 +1,6 @@
 module Metadata where
 
+import Data.List (union)
 import BV
 
 -- ^ Metadata
@@ -17,3 +18,11 @@ instance Measureable Expr where
 
 instance Measureable Program where
   size (Program x e)        = 1 + size e
+
+-- ^ Op function
+op (Const _) = []
+op (Id _) = []
+op (If0 e0 e1 e2) = ["if0"] `union` op e0 `union` op e1 `union` op e2
+op (Fold e0 e1 x y e2) = ["fold"] `union` op e0 `union` op e1 `union` op e2
+op (Op1 o e0) = [show o] `union` op e0
+op (Op2 o e0 e1) = [show o] `union` op e0 `union` op e1
