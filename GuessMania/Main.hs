5fa9db75ac67967cc67ff4d6ce3e3fb03c20095a
cutsea110 <cutsea110@gmail.com>
2013-08-11 17:12:40 +0900

    Update command argument

--- GuessMania/Main.hs
+++ GuessMania/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Control.Monad (forM_)
 import Data.Char (toLower)
 import qualified Data.Set as Set
 import Data.Set (Set)
@@ -8,6 +9,7 @@ import System.Exit
 import System.IO
 
 import BV
+import Interaction
 import Utility
 
 main :: IO ()
@@ -17,6 +19,13 @@ main = do
 
   args <- getArgs
   case args of
+    ["train", size, n] -> do
+      let (size', n') = (read size, read n)
+      forM_ [1..n'] $ \n -> trainTest [] size'
+    ["real", size] -> do
+      Just probs <- myproblems
+      let (probs', size') = (filter (\p -> probSize p == size') probs, read size)
+      forM_ probs' realTest
     [pid, ops', size'] -> do
       let ops  = words ops'
           size = read size'
@@ -33,6 +42,8 @@ main = do
         exitFailure
     _ -> do
       putStrLn "usage: GuessMania pid \"op1 op2 op3 .. \" size"
+      putStrLn "       GuessMania train size cycle"
+      putStrLn "       GuessMania real size"
       return ()
 
 allOps :: Set String
